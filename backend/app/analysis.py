c2337d12ee2e6e23b12c3c74659aeff07949ffe1
Usagi no Niku <chenbohan911@163.com>
2023-11-08 17:39:26 +0800

    backend: refactor analysis

--- backend/app/analysis.py
+++ backend/app/analysis.py
@@ -1,14 +1,11 @@
-from typing import Sequence
-
-from sqlalchemy import select, update, and_
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from sqlalchemy import select, update, and_, ScalarResult
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, async_object_session as object_session
 
 from app import database
 from app.database import engine
 from app.interaction import Score, Stage, StageMap, StageMapUser, StageUser, Beatmap
 
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
 
 def with_primary_key(analysis_dict: dict, **kwargs) -> dict:
     new_dict = {'analysis': analysis_dict}
@@ -17,117 +14,104 @@ def with_primary_key(analysis_dict: dict, **kwargs) -> dict:
     return new_dict
 
 
-# process: score_detail
-
-async def compute_score(score: Score) -> dict:
-    # TODO: real formula
+# score: one particular score.
+def process_score(score: Score) -> dict:
     beatmap: Beatmap = score.beatmap
-    return {
-        'percentage': score.highest_combo / beatmap.max_combo
-    }
+    return { 'percentage': score.highest_combo / beatmap.max_combo }
 
 
-async def compute_scores(session: AsyncSession, stage: Stage):
-    computed_results = []
-    scores = await session.scalars(select(Score).where(Score.stage_id == stage.id))  # type: ignore
-    for score in scores:
-        computed_result = with_primary_key(await compute_score(score), id=score.id)
-        computed_results.append(computed_result)
-    await session.execute(update(Score), computed_results)
+# scores: scores from the same user and the same map.
+def process_stage_entry(scores: ScalarResult[Score]) -> dict:
+    return { 'play_count': len(scores) }
 
 
-# process: stage_map_user_detail (smu: stage_map_user)
-
-async def compute_smu_scores(particular_scores: Sequence) -> dict:
-    # particular_scores: scores from the same user and beatmap.
-    # TODO: real formula
-    return {
-        'play_count': len(particular_scores)
-    }
+# scores: scores from different user and the same map.
+def process_stage_map(scores: ScalarResult[Score]) -> dict:
+    return { 'play_count': len(scores) }
 
 
-async def compute_smu_users(session: AsyncSession, stage: Stage, stage_map: StageMap) -> list[dict]:
-    computed_results = []
-    for user in stage.team.member:
-        condition = and_(Score.beatmap_md5 == stage_map.map_md5, Score.user_id == user.id,
-                         Score.stage_id == stage_map.stage_id)
-        current_scores = (await session.execute(select(Score).where(condition))).fetchall()
-        computed_result = with_primary_key(await compute_smu_scores(current_scores), stage_id=stage.id,
-                                           beatmap_md5=stage_map.map_md5, user_id=user.id)
-        computed_results.append(computed_result)
-    return computed_results
+# scores: scores from the same user and different maps.
+def process_stage_user(scores: ScalarResult[Score]) -> dict:
+    return { 'play_count': len(scores) }
 
 
-async def compute_smu(session: AsyncSession, stage: Stage):
-    computed_results = []
-    stage_maps = (await session.execute(stage.maps)).scalars().fetchall()
-    for stage_map in stage_maps:
-        computed_result = await compute_smu_users(session, stage, stage_map)
-        computed_results.extend(computed_result)
-    await session.execute(update(StageMapUser), computed_results)
+# stage: the stage to be processed.
+# stage_map: the analysis of each map in the stage.
+# stage_user: the analysis of each user in the stage.
+def process_stage(stage: Stage, stage_map: dict, stage_user: dict):
+    return {}
 
 
-# process: stage_map_detail
-
-async def compute_stage_map(particular_scores: Sequence) -> dict:
-    # particular_scores: scores from the same beatmap and different users.
-    # TODO: real formula
-    return {
-        'play_count': len(particular_scores)
-    }
-
-
-async def compute_stage_maps(session: AsyncSession, stage: Stage) -> list:
-    computed_results = []
-    stage_maps = (await session.execute(stage.maps)).scalars().fetchall()
+async def analyze_score(score: Score):
+    # score, score_entry: from bottom to top.
+    # stage, stage_map, stage_user: from top to bottom.
+    score.analysis = await process_score(score)
+    await analyze_stage_entry(score.beatmap_md5, score.user_id, score.stage_id)
+    await analyze_stage(score.stage)
+    
+    
+async def analyze_stage_entry(session: AsyncSession, beatmap_md5: str, user_id: int, stage_id: int) -> dict:
+    condition = and_(Score.beatmap_md5 == beatmap_md5, Score.user_id == user_id, Score.stage_id == stage_id)
+    scores = (await session.execute(select(Score).where(condition))).scalars()
+    sentence = with_primary_key(await process_stage_entry(scores), stage_id=stage_id, beatmap_md5=beatmap_md5, user_id=user_id)
+    await session.execute(update(StageMapUser), sentence)
+    
+    
+async def analyze_stage_map(stage: Stage) -> list[dict]:
+    session = object_session(stage)
+    sentences = []
+    stage_maps = (await session.execute(stage.maps)).scalars()
     for stage_map in stage_maps:
         condition = and_(StageMapUser.stage_id == stage.id, StageMapUser.beatmap_md5 == stage_map.map_md5)
-        current_scores = (await session.execute(select(StageMapUser).where(condition))).fetchall()
-        computed_result = with_primary_key(await compute_stage_map(current_scores), stage_id=stage.id,
-                                           map_md5=stage_map.beatmap_md5)
-        computed_results.append(computed_result)
-    await session.execute(update(StageMap), computed_results)
-    return computed_results
+        scores = (await session.execute(select(StageMapUser).where(condition))).scalars()
+        sentences.append(with_primary_key(await process_stage_map(scores), stage_id=stage.id, map_md5=stage_map.beatmap_md5))
+    await session.execute(update(StageMap), sentences)
+    return sentences
 
 
-# process: stage_user_detail
-
-async def compute_stage_user(particular_scores: Sequence) -> dict:
-    # particular_scores: scores from the same user and different maps.
-    # TODO: real formula
-    return {
-        'play_count': len(particular_scores)
-    }
-
-
-async def compute_stage_users(session: AsyncSession, stage: Stage) -> list:
-    computed_results = []
+async def analyze_stage_user(stage: Stage) -> list[dict]:
+    session = object_session(stage)
+    sentences = []
     for user in stage.team.member:
         condition = and_(StageMapUser.stage_id == stage.id, StageMapUser.user_id == user.id)
-        current_scores = (await session.execute(select(StageMapUser).where(condition))).fetchall()
-        computed_result = with_primary_key(await compute_stage_user(current_scores), stage_id=stage.id, user_id=user.id)
-        computed_results.append(computed_result)
-    await session.execute(update(StageUser), computed_results)
-    return computed_results
+        scores = (await session.execute(select(StageMapUser).where(condition))).scalars
+        sentences.append(with_primary_key(await process_stage_user(scores), stage_id=stage.id, user_id=user.id))
+    await session.execute(update(StageUser), sentences)
+    return sentences
 
 
-# process: stage_detail
-async def compute_stage(stage: Stage, stage_maps: list[dict], stage_users: list[dict]):
-    total_play_count = 0
+async def analyze_stage(stage: Stage):
+    stage_map = await analyze_stage_map(stage)
+    stage_user = await analyze_stage_user(stage)
+    stage.analysis = await process_stage(stage, stage_map, stage_user)
+
+
+# analyze the scores of a stage from scratch.
+async def analyze_scores_scratch(stage: Stage):
+    session = object_session(stage)
+    scores = await session.scalars(select(Score).where(Score.stage_id == stage.id))  # type: ignore
+    stage_maps = (await session.execute(stage.maps)).scalars()
+    
+    # analyze the scores itself
+    sentences = [with_primary_key(await process_score(score), id=score.id) for score in scores]
+    await session.execute(update(Score), sentences)
+    
+    sentences = []
+    
+    # analyze the scores stage_entry
     for stage_map in stage_maps:
-        total_play_count += stage_map.get('play_count')
-    computed_result = {
-        'play_count': total_play_count
-    }  # TODO: real formula
-    stage.analysis = computed_result
-
-
-async def process_async(stage_id: int):
+        for user in stage.team.member:
+            condition = and_(Score.beatmap_md5 == stage_map.map_md5, Score.user_id == user.id, Score.stage_id == stage_map.stage_id)
+            stage_entry_scores = (await session.execute(select(Score).where(condition))).scalars()
+            sentences.append(with_primary_key(await process_stage_entry(stage_entry_scores), stage_id=stage.id, beatmap_md5=stage_map.map_md5, user_id=user.id))
+    await session.execute(update(StageMapUser), sentences)
+    
+    
+async def analyze_stage_scratch(session: AsyncSession, stage_id: int):
     session = async_session_maker()
-    stage: Stage = await database.get_model(session, stage_id, Stage)
-    await compute_scores(session, stage)  # score_detail
-    await compute_smu(session, stage)  # stage_map_user_detail
-    cached_maps = await compute_stage_maps(session, stage)  # stage_map_detail
-    cached_users = await compute_stage_users(session, stage)  # stage_user_detail
-    await compute_stage(stage, cached_maps, cached_users)  # stage_detail
-    await session.commit()
+    stage = await database.get_model(session, stage_id, Stage)
+    # score, score_entry, stage, stage_map, stage_user: from top to bottom.
+    await analyze_scores_scratch(session, stage) # revert order in scratch method.
+    await analyze_stage_map(stage)
+    await analyze_stage_user(stage)
+    stage.analysis = await process_stage(stage)
