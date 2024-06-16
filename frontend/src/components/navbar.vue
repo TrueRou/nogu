5281d2c6298f399b3d293bca097041e0c1f5c74f
Ari Arimaniac <arily@MacBook-Pro-2.local>
2024-06-16 16:39:30 +0800

    fix navbar imperfection

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -48,7 +48,7 @@ function onScroll() {
         <div class="navbar">
             <div class="x-spacer"></div>
             <div class="navbar-start">
-                <RouterLink class="relative p-1 text-lg normal-case btn btn-ghost rounded-3xl h-10 min-h-fit"
+                <RouterLink class="relative h-10 p-1 text-lg btn btn-ghost rounded-3xl min-h-fit"
                     :to="currentGame.route">
                     <img v-if="hasFeaturedGame" :src="currentGame.icon" class="h-7">
                     <span class="nogu-brand-small">NOGU</span>
@@ -56,26 +56,26 @@ function onScroll() {
             </div>
             <div class="navbar-center">
                 <template v-if="hasFeaturedGame">
-                    <RouterLink class="text-lg font-normal normal-case btn btn-ghost rounded-3xl h-10 min-h-fit"
+                    <RouterLink class="h-10 text-lg btn btn-ghost rounded-3xl min-h-fit"
                         :to="currentGame.route + '/discover'">
-                        <i class="fa-solid fa-globe"></i>
+                        <i class="fa-solid fa-globe" />
                         <b>Discover</b>
                     </RouterLink>
                 </template>
             </div>
-            <div class="navbar-end">
+            <div class="items-center navbar-end">
                 <RouterLink
-                    class="w-10 h-10 p-1 text-lg font-normal normal-case rounded-full btn btn-ghost btn-circle min-h-fit"
+                    class="text-lg btn btn-ghost btn-circle min-h-fit"
                     to="/">
-                    <i class="fa-solid fa-search"></i>
+                    <i class="fa-solid fa-search" />
                 </RouterLink>
-                <div class="h-10 dropdown dropdown-end">
-                    <label tabindex="0" class="w-10 p-0 btn btn-ghost btn-circle avatar h-fit min-h-fit">
+                <div class="dropdown dropdown-end">
+                    <div role="button" tabindex="0" class="btn btn-ghost btn-circle avatar">
                         <div class="rounded-full w-9">
                             <img v-if="session.isLoggedIn" src="https://a.ppy.sb/1094" />
                             <img v-else="session.isLoggedIn" src="https://a.ppy.sb/-1" />
                         </div>
-                    </label>
+                    </div>
                     <ul tabindex="0"
                         class="mt-3 z-[1] p-2 shadow menu menu-md dropdown-content bg-neutral rounded-box w-52">
                         <template v-if="session.isLoggedIn">
